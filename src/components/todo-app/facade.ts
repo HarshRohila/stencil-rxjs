9d6b751ccc28682e28a722be40f84b23435f85a3
Harsh Rohila <rohilaharsh@gmail.com>
2023-04-19 19:32:22 +0530

    added todo app

---
+++ src/components/todo-app/facade.ts
@@ -0,0 +1,78 @@
+import { BehaviorSubject, Subject, map, takeUntil } from 'rxjs'
+
+interface State {
+  todos: Todo[]
+  inputText: string
+}
+
+export interface Todo {
+  id: number
+  text: string
+}
+
+const initialState: State = {
+  todos: [
+    {
+      id: 1,
+      text: 'First Todo',
+    },
+    {
+      id: 2,
+      text: 'Second Todo',
+    },
+  ],
+  inputText: '',
+}
+
+const createTodo = (function () {
+  let lastId = 2
+
+  return function (text: string) {
+    return {
+      id: ++lastId,
+      text,
+    } as Todo
+  }
+})()
+
+const setState = (partialState: Partial<State>) => {
+  state$.next({ ...state$.value, ...partialState })
+}
+
+export const events = {
+  createAddTodoEvent(disconnected$: Subject<void>) {
+    const addTodo$ = new Subject<void>()
+
+    addTodo$
+      .pipe(
+        map(() => state$.value.inputText),
+        map(text => createTodo(text)),
+        takeUntil(disconnected$),
+      )
+      .subscribe(newTodo => {
+        setState({ todos: [...state$.value.todos, newTodo] })
+      })
+
+    return {
+      emit() {
+        addTodo$.next()
+      },
+    }
+  },
+
+  createInputTextChangedEvent(disconnected$: Subject<void>) {
+    const inputTextChanged$ = new Subject<string>()
+
+    inputTextChanged$.pipe(takeUntil(disconnected$)).subscribe(text => {
+      setState({ inputText: text })
+    })
+
+    return {
+      emit(inputText: string) {
+        inputTextChanged$.next(inputText)
+      },
+    }
+  },
+}
+
+export const state$ = new BehaviorSubject<State>(initialState)
---
+++ src/components/todo-app/todo-app.tsx
@@ -0,0 +1,55 @@
+import { Component, Host, h, State } from '@stencil/core'
+import { events, state$, Todo } from './facade'
+import { Subject, takeUntil } from 'rxjs'
+
+@Component({
+  tag: 'todo-app',
+  shadow: true,
+})
+export class TodoApp {
+  @State() todos: Todo[]
+
+  disconnected$ = new Subject<void>()
+
+  addTodoEvent = events.createAddTodoEvent(this.disconnected$)
+  inputTextChangedEvent = events.createInputTextChangedEvent(this.disconnected$)
+
+  componentWillLoad() {
+    state$.pipe(takeUntil(this.disconnected$)).subscribe(state => {
+      this.todos = state.todos
+    })
+  }
+
+  disconnectedCallback() {
+    this.disconnected$.next()
+  }
+
+  private handleAddTodo = (ev: Event) => {
+    ev.preventDefault()
+    this.addTodoEvent.emit()
+  }
+
+  private handleInputTextChange = (ev: Event) => {
+    const target = ev.target as HTMLInputElement
+    this.inputTextChangedEvent.emit(target.value)
+  }
+
+  render() {
+    return (
+      <Host>
+        <form onSubmit={this.handleAddTodo}>
+          <input type="text" placeholder="New Todo" onInput={this.handleInputTextChange} />
+          <button>Add Todo</button>
+        </form>
+
+        <ul>
+          {this.todos.map(t => (
+            <li>
+              <h3>{t.text}</h3>
+            </li>
+          ))}
+        </ul>
+      </Host>
+    )
+  }
+}
